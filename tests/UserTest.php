7b05ccbcdb9e23bec66e36564e9799a3e5c170c9
Blair Fraser <s3641830@student.rmit.edu.au>
2019-04-01 01:01:44 +1100

    Reconstructed most of lost work - tests cover user object core functionality, getEmailExists and all static methods other than getUserLogin, although getUserByVerifyCode test isn't working currently

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,7 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 final class UserTest extends TestCase {
 
-    //Actually can't use the below as the tearDown function is run per-test, not per test class as I'd thought
     private $idsToDelete = array();
 
     public function testConstructorUserIdZeroGivenZero() {
@@ -18,7 +17,8 @@ final class UserTest extends TestCase {
     }
 
     public function testConstructorDBServerInaccessible() {
-
+        //not yet implemented
+        $this->markTestIncomplete();
     }
 
     public function testSaveEmailExists() {
@@ -36,7 +36,6 @@ final class UserTest extends TestCase {
         $user->email = "unit@tester.com";
         $user->active = 1;
         $user->password = NULL;
-        $user->verifyCode = '65630340-1718-4A7E-8EE3-39DC3D448ED0';
         $user->verified = 0;
         $user->enteredDetails = 0;
         $user->resetCode = NULL;
@@ -57,8 +56,12 @@ final class UserTest extends TestCase {
         $oid2 = $objSave->objectId;
         $user2 = new \Classes\User($oid2);
         //Cleanup DB BEFORE assertions as it seems code after them doesn't get executed (according to hacky var_dump testing)
-        $this->cleanup($oid);
-        $this->cleanup($oid2);
+        //$this->cleanup($oid);
+        //$this->cleanup($oid2);
+
+        $this->idsToDelete[] = $oid;
+        $this->idsToDelete[] = $oid2;
+
         $this->assertEquals($oid, $oid2);
         $this->assertEquals("reallyunittesting@test.com", $user2->email);
     }
@@ -68,17 +71,126 @@ final class UserTest extends TestCase {
         $user = new \Classes\User($oid);
         //var_dump("Testing with oid ".$oid);
         //Cleanup DB BEFORE assertions as it seems code after them doesn't get executed (according to hacky var_dump testing)
-        $this->cleanup($oid);
+        //$this->cleanup($oid);
+
+        $this->idsToDelete[] = $oid;
+
         $this->assertEquals('unit@tester.com', $user->email);
         $this->assertEquals(NULL, $user->password);
         $this->assertEquals(1, $user->userType);
         $this->assertEquals(1, $user->active);
-        $this->assertEquals('65630340-1718-4A7E-8EE3-39DC3D448ED0', $user->verifyCode);
         $this->assertEquals(0, $user->verified);
         $this->assertEquals(0, $user->enteredDetails);
         $this->assertEquals(NULL, $user->resetCode);
     }
 
+    public function testGetEmailExists() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $this->assertSame(true, $user->GetEmailExists($user->email, 0));
+    }
+
+    public function testGetEmailExistsFailure() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $this->assertSame(false, $user->GetEmailExists(NULL, 0));
+    }
+
+    public function testGetUserByVerifyCode() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $user->verified = false;
+        $user->active = true;
+        $user->save();
+        $this->assertEquals($user, \Classes\User::GetUserByVerifyCode($user->verifyCode));
+    }
+
+    public function testGetUserByVerifyCodeFailure() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $this->assertEquals(null, \Classes\User::GetUserByVerifyCode($user->verifyCode));
+    }
+
+    public function testGetUserByEmailAddress() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $user->verified = true;
+        $user->active = true;
+        $user->save();
+        $this->assertEquals($user, \Classes\User::GetUserByEmailAddress($user->email));
+    }
+
+    public function testGetUserByEmailAddressFailure() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $this->assertEquals(null, \Classes\User::GetUserByEmailAddress($user->email));
+    }
+
+    public function testGetUserByResetCode() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $user->verified = true;
+        $user->active = true;
+        $user->save();
+        $this->assertEquals($user, \Classes\User::GetUserByResetCode($user->resetCode));
+    }
+
+    public function testGetUserByResetCodeFailureNoMatch() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $this->assertEquals(null, \Classes\User::GetUserByResetCode($user->resetCode));
+    }
+
+    public function testGetUserByResetCodeFailurecodeLength() {
+        $oid = $this->saveNewUser();
+        $user = new \Classes\User($oid);
+        $this->idsToDelete[] = $oid;
+        $user->verified = true;
+        $user->active = true;
+        $user->save();
+        $this->assertEquals(null, \Classes\User::GetUserByResetCode("123"));
+    }
+
+    public function testGetUserLogin() {
+        
+    }
+
+    protected function setUp(): void {
+        parent::setUp();
+        $this->idsToDelete = array();
+    }
+
+    protected function tearDown(): void {
+        $conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or die("Connection failed: " . $conn->connect_error);
+        //var_dump($conn);
+        //var_dump("In cleanup with oid ".$oid);
+        foreach ($this->idsToDelete as $idd) {
+            $sql = 'delete from user where UserId = ?';
+            if ($stmt = $conn->prepare($sql)) {
+                //var_dump($stmt);
+                $stmt->bind_param("i", $idd);
+                //var_dump("Cleaning up - deleting user with id ".$oid);
+                $stmt->execute();
+                $result = mysqli_stmt_get_result($stmt);
+                $stmt->close();
+            } else {
+                var_dump($errorMessage = $conn->errno . ' ' . $conn->error);
+            }
+        }
+        $conn->close();
+        parent::tearDown();
+    }
+
+
+    /**
     private function cleanup($oid) {
         
         //delete record created by test process
@@ -100,6 +212,8 @@ final class UserTest extends TestCase {
         }
         $conn->close();
     }
+
+    */
 
 }
 
