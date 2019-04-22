eeb922483f3af2565eb8cd4a9d6e14d1e7327dc3
Shane <40532070+Shane-West@users.noreply.github.com>
2019-04-23 07:35:10 +1000

    Added delete job modal

--- create_job.php
+++ create_job.php
@@ -330,12 +330,35 @@
 								}
 							?>
 						
-							<button class="btn btn-primary"><?php echo $submitBtn; ?></button>  
+							<button class="btn btn-primary"><?php echo $submitBtn; ?></button>
 						</div>
 					</div>
 				</div>
 				
 			</form>
+			<?php if($submitBtn == "Save"){ ?>
+				<!-- Delete job button -->
+				<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteJob">Delete Job</button>
+				
+				<!-- Delete Job modal -->
+				<div class="modal fade" id="deleteJob">
+					<div class="modal-dialog modal-sm">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="deleteJob">Delete Job?</h5>
+								<button type="button" class="close" data-dismiss="modal" aria-label="Close">&times;</button>
+							</div>
+							<div class="modal-body">
+								<p>Are you sure you want to delete this job?</p>
+							</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+								<button type="button" class="btn btn-danger">Delete Job</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
 			
 		</section>
     
