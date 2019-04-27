c4791adde9e42c0ae9778c2852d91db1197454d5
Shane <40532070+Shane-West@users.noreply.github.com>
2019-04-28 08:17:41 +1000

    Update view_job_seeker.php
    Moved some items around, and hid some items if they're empty

--- view_job_seeker.php
+++ view_job_seeker.php
@@ -59,103 +59,137 @@
 				</div>
 			</div>
 			
+			<!-- Location and field -->
+			<div class="row">
+				<div class="col-sm-6">
+					<p><strong>Location: </strong><?php echo htmlspecialchars($state); ;?></p>
+				</div>
+				
+				<div class="col-sm-6">
+					<p><strong>Expertise: </strong><?php echo htmlspecialchars($skillCategoryName);?></p>
+				</div>
+			</div>
+			
 			<!-- Contact Details -->
-			<div class="row mt-2">
+			<div class="row mt-4">
 				<div class="col-sm-12">
 					<h3>Contact Details</h3>
 				</div>
 			</div>
-			<br>
 			
 			<!-- Job Seeker Contact Details -->
-			<div class="row">
-				<div class="col-sm-2">
-					<p><strong>Area Code: </strong><?php echo htmlspecialchars($phoneAreaCode); ?></p>
+			<div class="row mt-2">
+				<div class="col-sm-6">
+					<p><strong>Phone Number: </strong><?php echo "(" . htmlspecialchars($phoneAreaCode) . ") " . htmlspecialchars($phoneNumber); ?></p>
 				</div>
-				<div class="col-sm-5">
-					<p><strong>Phone Number: </strong><?php echo htmlspecialchars($phoneNumber); ?></p>
-				</div>
-				<div class="col-sm-5">
+				
+				<div class="col-sm-6">
 					<p><strong>Mobile Number: </strong><?php echo htmlspecialchars($mobileNumber); ?></p>
 				</div>
-				<div class="col-sm-5">
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-12">
 					<p><strong>Address 1: </strong><?php echo htmlspecialchars($address1); ?></p>
 				</div>
-				<div class="col-sm-5">
-					<p><strong>Address 2: </strong><?php echo htmlspecialchars($address2); ?></p>
-				</div>
-				<div class="col-sm-3">
-					<p><strong>Post code: </strong><?php echo htmlspecialchars($postcode); ?></p>
-				</div>
-				<div class="col-sm-6">
+				<?php if(strlen(htmlspecialchars($address2)) > 0){ ?>
+					<div class="col-sm-12">
+						<p><strong>Address 2: </strong><?php echo htmlspecialchars($address2); ?></p>
+					</div>
+				<?php } ?>
+			</div>
+			
+			<div class="row">
+				<div class="col-sm-4">
 					<p><strong>City: </strong><?php echo htmlspecialchars($city); ?></p>
 				</div>
-				<div class="col-sm-3">
+				
+				<div class="col-sm-4">
 					<p><strong>State: </strong><?php echo htmlspecialchars($state); ?></p>
+				</div>
+				
+				<div class="col-sm-4">
+					<p><strong>Post code: </strong><?php echo htmlspecialchars($postcode); ?></p>
 				</div>
 			</div>
 			
 			<!-- Additional Info -->
-			<div class="row mt-2">
+			<div class="row mt-4">
 				<div class="col-sm-12">
 					<h3>Additional Info</h3>
 				</div>
 			</div>
-			<br>
 			
 			<!-- Job Seeker Additional Info -->
-			<div class="row mt-6">
-				<div class="col-sm-3">
+			<div class="row mt-2">
+				<div class="col-sm-6">
 					<p><strong>Age Group: </strong><?php echo htmlspecialchars($ageGroup); ?></p>
 				</div>
+				
 				<div class="col-sm-6">
-					<p><strong>Actively Looking?: </strong><?php echo htmlspecialchars($active); ?></p>
+					<p>
+						<strong>Actively Looking: </strong>
+						<?php 
+							if ($active == 1){
+								echo "Yes";
+							}
+							else {
+								echo "No";
+							}
+						?>
+					</p>
 				</div>
+				
+				<div class="col-sm-6">
+					<p><strong>Sign Up Reason: </strong><?php echo htmlspecialchars($signUpReason); ?></p>
+				</div>
+				
+				<div class="col-sm-6">
+					<p><strong>Availability: </strong><?php echo htmlspecialchars($jobChangeSpeed); ?></p>
+				</div>				
 			</div>
 			
 			<!-- Education -->
-			<div class="row mt-2">
+			<div class="row mt-4">
 				<div class="col-sm-12">
-					<h3>Education</h3>
+					<h4>Education</h4>
 				</div>
 			</div>
-			<br>
 			
 			<!-- Job Seeker Education -->
-			<div class="row mt-6">
+			<div class="row mt-2">
 				<div class="col-sm-8">
 					<p><strong>Highest Level of schooling completed: </strong><?php echo htmlspecialchars($highestLevelCompleted); ?></p>
 				</div>
 			</div>
+			
 			<div class="row mt-6">
 				<div class="col-sm-3">
 					<p><strong>Currently Studying: </strong><?php echo htmlspecialchars($currentlyStudying); ?></p>
 				</div>
-				<div class="col-sm-5">
-					<p><strong>Currently Studying Level: </strong><?php echo htmlspecialchars($currentStudyLevel); ?></p>
-				</div>
-				<div class="col-sm-6">
-					<p><strong>Sign Up Reason: </strong><?php echo htmlspecialchars($signUpReason); ?></p>
-				</div>
-				<div class="col-sm-6">
-					<p><strong>Job Change Speed: </strong><?php echo htmlspecialchars($jobChangeSpeed); ?></p>
-				</div>
+				
+				<?php if($currentlyStudying == "YES"){ ?>
+					<div class="col-sm-5">
+						<p><strong>Current Study Level: </strong><?php echo htmlspecialchars($currentStudyLevel); ?></p>
+					</div>
+				<?php } ?>
+				
 			</div>
 			
 			<!-- Skills -->
-			<div class="row mt-2">
+			<div class="row mt-4">
 				<div class="col-sm-12">
-					<h3>Skills</h3>
+					<h4>Skills</h4>
 				</div>
 			</div>
-			<br>
 			
 			<!-- Skills Category and loop skills-->
-			<div class="row mt-6">
+			<div class="row mt-2">
 				<div class="col-sm-6">
-						<p><strong>Skill Category: </strong><?php echo htmlspecialchars($skillCategoryName); ?><p>
+						<p><strong>Expertise: </strong><?php echo htmlspecialchars($skillCategoryName); ?><p>
 				</div>
 			</div>
+			
 			<div id="skillsSection">
 				<div class="col-sm-12">
 					<?php					
