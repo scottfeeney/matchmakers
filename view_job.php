7ceffe587cd6ee29908c3df692fda034a4068382
danny1188 <s3669208@student.rmit.edu.au>
2019-04-30 17:56:03 +1000

    fixes for mobile job view

--- view_job.php
+++ view_job.php
@@ -38,14 +38,14 @@
 
 	// Associative array of maps for each location
 	$maps = array();
-	$maps['Adelaide'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-34.9242062953014~138.59462275247796&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
-	$maps['Brisbane'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-27.468007725655156~153.0220862218403&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8" ;
-	$maps['Canberra'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-35.30746246270736~149.12195478297167&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
-	$maps['Darwin'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-12.457115077105016~130.8404146521356&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
-	$maps['Hobart'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-42.88270972627814~147.33020267077376&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
-	$maps['Melbourne'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-37.81607137280399~144.9636397932391&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
-	$maps['Perth'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-31.913550691161007~115.88006194312811&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
-	$maps['Sydney'] = "https://www.bing.com/maps/embed?h=400&w=400&cp=-33.86960029335556~151.2059159744571&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";	
+	$maps['Adelaide'] = "cp=-34.9242062953014~138.59462275247796&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
+	$maps['Brisbane'] = "cp=-27.468007725655156~153.0220862218403&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8" ;
+	$maps['Canberra'] = "cp=-35.30746246270736~149.12195478297167&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
+	$maps['Darwin'] = "cp=-12.457115077105016~130.8404146521356&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
+	$maps['Hobart'] = "cp=-42.88270972627814~147.33020267077376&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
+	$maps['Melbourne'] = "cp=-37.81607137280399~144.9636397932391&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
+	$maps['Perth'] = "cp=-31.913550691161007~115.88006194312811&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";
+	$maps['Sydney'] = "cp=-33.86960029335556~151.2059159744571&lvl=13&typ=d&sty=r&src=SHELL&FORM=MBEDV8";	
 
 	// Display header section	
 	$header = new \Template\Header();
@@ -57,27 +57,25 @@
 			<!-- Position name -->
 			<div class="row">
 				<div class="col-sm-12">
-					<h2>
-						<?php echo htmlspecialchars($positionName);
-							if($numberAvailable > 1){
-								echo "<span id='jobQtyBadge' class='badge badge-success'>$numberAvailable</span>";
+					<h2 class="job-view-title"><?php echo htmlspecialchars($positionName); ?></h2>					
+					<?php 	if($numberAvailable > 1){
+								echo '<span class="badge badge-success job-positions">' . $numberAvailable . ' positions</span>';
 							}
 						?>
-					</h2>					
 				</div>
 			</div>
 			
 			<!-- Company name -->
 			<div class="row mt-2">
 				<div class="col-sm-12">
-					<h3><?php echo htmlspecialchars($companyName); ?></h3>
+					<p><strong>Employer: </strong><?php echo $companyName; ?></p>
 				</div>
 			</div>
 			
 			<!-- Job Type, Start period, and Reference No.-->
 			<div class="row mt-2">
 				<div class="col-sm-4">
-					<p><strong>Job Type: </strong><?php echo $jobTypeName; ?><p>
+					<p><strong>Job Type: </strong><?php echo $jobTypeName; ?></p>
 				</div>
 				<div class="col-sm-4">
 					<p><strong>Start Period: </strong><?php echo $positionAvailability; ?></p>
@@ -102,7 +100,7 @@
 					<?php					
 						// Loop through array of skills to display skill name
 						foreach($selectedSkills as $skill){
-							echo "<span class='badge badge-info jobSkillDisplay'>$skill->skillName</span>";
+							echo "<span class='badge badge-info jobSkillDisplay'>" . htmlspecialchars($skill->skillName) . "</span>";
 						}
 					?>
 				</div>
@@ -113,11 +111,12 @@
 				<div>
 					<h2>Location: <?php echo $locationName; ?></h2>
 					
-					<div>
-						<iframe width="400" 
-								height="400" 
-								src=<?php echo $maps[$locationName]; ?> 
-								scrolling="no">
+					<div class="map-container">
+						<iframe width="485" 
+								height="300" 
+								src="<?php echo "https://www.bing.com/maps/embed?h=300&w=485&" . $maps[$locationName]; ?>"
+								scrolling="no"
+								frameborder="0" >
 						</iframe>
 					</div>
 				</div>
